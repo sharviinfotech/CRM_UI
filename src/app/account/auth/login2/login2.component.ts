11a582249a2aa1eaf2f45263672dfc05ee6d57bc
kvvk07 <kvvkkunchala@gmail.com>
2025-09-22 16:19:05 +0530

    logo changed

--- src/app/account/auth/login2/login2.component.ts
+++ src/app/account/auth/login2/login2.component.ts
@@ -32,9 +32,9 @@ export class Login2Component implements OnInit {
   images = [
     
     // 'assets/images/AircraftFlight.png',
-    'assets/images/bgrdashboard.png',
-    'assets/images/HBL_background_resized_1330x780.png',
-     'assets/images/HBL_lightImage.png',
+    'assets/images/image (3).png',
+    // 'assets/images/HBL_background_resized_1330x780.png',
+    //  'assets/images/HBL_lightImage.png',
    
   ];
   currentIndex = 0;
